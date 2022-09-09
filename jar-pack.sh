1ab6f397df0d30a7c68108939ef0ba68d784f744
Melissa Linkert <melissa@glencoesoftware.com>
2022-09-09 16:56:36 -0500

    Fix paths to downloaded artifacts

--- jar-pack.sh
+++ jar-pack.sh
@@ -2,10 +2,6 @@
 
 set -x
 
-# extract downloaded artifacts for each platform
-unzip artifacts-linux-x86_64.zip
-unzip artifacts-macos-x86_64.zip
-unzip artifacts-windows-x86_64.zip
 cd build/libs
 # unpack the Java-only jar
 jar xvvf libjpeg-turbo*.jar
@@ -14,11 +10,11 @@ jar xvvf libjpeg-turbo*.jar
 # https://github.com/scijava/native-lib-loader
 # and move the corresponding library file
 mkdir -p META-INF/lib/windows_64
-mv ../../Debug/turbojpeg.dll META-INF/lib/windows_64/
+mv ../../artifacts-windows-x86_64/Debug/turbojpeg.dll META-INF/lib/windows_64/
 mkdir -p META-INF/lib/osx_64
-mv ../../libturbojpeg.dylib META-INF/lib/osx_64/
+mv ../../artifacts-macos-x86_64/libturbojpeg.dylib META-INF/lib/osx_64/
 mkdir -p META-INF/lib/linux_64
-mv ../../libturbojpeg.so META-INF/lib/linux_64/
+mv ../../artifacts-linux-x86_64/libturbojpeg.so META-INF/lib/linux_64/
 
 # repack the jar file to include the native libraries
 jar uvvf libjpeg-turbo*.jar META-INF/lib/*
