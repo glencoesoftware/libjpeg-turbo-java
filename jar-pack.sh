d7f2129acbbb925764f216241205b0e6aa81ee44
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-12-13 18:37:31 +0000

    arm build scripts

--- jar-pack.sh
+++ jar-pack.sh
@@ -15,6 +15,10 @@ mkdir -p META-INF/lib/osx_64
 mv ../../artifacts-macos-x86_64/libturbojpeg.dylib META-INF/lib/osx_64/
 mkdir -p META-INF/lib/linux_64
 mv ../../artifacts-linux-x86_64/libturbojpeg.so META-INF/lib/linux_64/
+mkdir -p META-INF/lib/osx_arm64
+mv ../../artifacts-macos-arm64/libturbojpeg-arm64.dylib META-INF/lib/osx_arm64/libturbojpeg.dylib
+mkdir -p META-INF/lib/linux_arm64
+mv ../../artifacts-linux-arm64/libturbojpeg-arm64.so META-INF/lib/linux_arm64/libturbojpeg.so
 
 # repack the jar file to include the native libraries
 jar uvvf libjpeg-turbo*.jar META-INF/lib/*
